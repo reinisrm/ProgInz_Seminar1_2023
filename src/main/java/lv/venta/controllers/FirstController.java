c41f59f7faf4e969926bfc78036c73223b9f0083
reinisrm <rreeiinniiss@gmail.com>
2023-05-25 00:05:17 +0300

    Add: new service functions

--- src/main/java/lv/venta/controllers/FirstController.java
+++ src/main/java/lv/venta/controllers/FirstController.java
@@ -3,6 +3,7 @@ package lv.venta.controllers;
 import java.util.ArrayList;
 import java.util.Arrays;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,56 +12,60 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import lv.venta.models.Product;
+import lv.venta.services.iCRUDProductService;
+import lv.venta.services.iFilteringProductService;
+
 @Controller
 public class FirstController {
-	
-	
-	private ArrayList<Product> allProducts
-	= new ArrayList<>(Arrays.asList(
-			new Product("Abols", 3.99f, "Sarkans", 3),
-			new Product("Burkans", 0.33f, "Oranzs", 2),
-			new Product("Gurkis", 1.22f, "Zals", 6)));
-	@GetMapping("/hello") //tiks izsaukta, ja url bus localhost:8080/hello
+	@Autowired
+	private iFilteringProductService filterService;
+	@Autowired
+	private iCRUDProductService crudService;
+
+	@GetMapping("/hello") // tiks izsaukta, ja url bus localhost:8080/hello
 	public String helloFunc() {
 		System.out.println("Mans pirmais kontrolieris ir nostradajis");
-		return "hello-page"; //tiks paradita hello-page.html lapa
+		return "hello-page"; // tiks paradita hello-page.html lapa
 	}
-	
-	@GetMapping("/msg") //localhost:8080/msg
+
+	@GetMapping("/msg") // localhost:8080/msg
 	public String msgFunc(Model model) {
 		model.addAttribute("myMsg", "Te zina no Reina");
 		model.addAttribute("myDate", "23.03.2023");
-		return "msg-page";//tiks paradita msg-page.html lapa un taja paradisies ari mmyMsg un myDate
-		
+		return "msg-page";// tiks paradita msg-page.html lapa un taja paradisies ari mmyMsg un myDate
+
 	}
-	
-	@GetMapping("/product") //localhost:8080/product
+
+	@GetMapping("/product") // localhost:8080/product
 	public String prodFunc(Model model) {
 		Product prod = new Product("Abols", 3.99f, "Sarkans", 3);
 		model.addAttribute("myProduct", prod);
 		return "product-page";
 	}
-	
-	@GetMapping("/productOne") //localhost:8080/productOne?title=Abols
+
+	@GetMapping("/productOne") // localhost:8080/productOne?title=Abols
 	public String productByParamFunc(@RequestParam("title") String title, Model model) {
-		if(title!=null) {
-			for(Product temp: allProducts) {
-				if(temp.getTitle().equals(title)) {
-					model.addAttribute("myProduct", temp);
-					return "product-page";
-				}
+		if (title != null) {
+			Product temp;
+			try {
+				temp = crudService.retrieveOneProductByTitle(title);
+				model.addAttribute("myProduct", temp);
+				return "product-page";
+			} catch (Exception e) {
+				return "error-page"; // paradis error-page.html lapu
 			}
-		}
-		return "error-page"; // paradis error-page.html lapu
+
+		} return "error-page"; // paradis error-page.html lapu
+
 	}
-	
-	//TODO localhost:8080/product/abols
-	
-	@GetMapping("/product/{title}") //localhost:8080/productOne?title=Abols
+
+	// TODO localhost:8080/product/abols
+
+	@GetMapping("/product/{title}") // localhost:8080/productOne?title=Abols
 	public String productByParamFunc2(@PathVariable("title") String title, Model model) {
-		if(title!=null) {
-			for(Product temp: allProducts) {
-				if(temp.getTitle().equals(title)) {
+		if (title != null) {
+			for (Product temp : allProducts) {
+				if (temp.getTitle().equals(title)) {
 					model.addAttribute("myProduct", temp);
 					return "product-page";
 				}
@@ -68,78 +73,79 @@ public class FirstController {
 		}
 		return "error-page"; // paradis error-page.html lapu
 	}
-	//kontrolieris, kas atgriez visus produktus
-	@GetMapping("/allProducts") //localhost:8080/allProducts
+
+	// kontrolieris, kas atgriez visus produktus
+	@GetMapping("/allProducts") // localhost:8080/allProducts
 	public String allProductsFunc(Model model) {
 		model.addAttribute("myAllProducts", allProducts);
 		return "all-products-page";
 	}
-	//kontrolieris, kas izfiltre visus produktus, kuru cena ir mazaka par noteiktu
-	@GetMapping("/allProducts/{price}") //localhost:8080/allProducts/1.99
+
+	// kontrolieris, kas izfiltre visus produktus, kuru cena ir mazaka par noteiktu
+	@GetMapping("/allProducts/{price}") // localhost:8080/allProducts/1.99
 	public String allProductsByPrice(@PathVariable("price") float price, Model model) {
-		if(price > 0) {
-			ArrayList<Product> allProductsWithPriceLess = new ArrayList<>();
-			for(Product temp: allProducts) {
-				if(temp.getPrice() < price) {
-					allProductsWithPriceLess.add(temp);
-					
-				}
-			}
+		ArrayList<Product> allProductsWithPriceLess = filterService.filterByPriceLess(price);
 			model.addAttribute("myAllProducts", allProductsWithPriceLess);
 			return "all-products-page";
-		}
-		return "error-page";
+		
+		//return "error-page";
 	}
-	
-	@GetMapping("/insert") //localhost:8080/insert
-	public String insertProductFunc(Product product) {  //tiek padots tuksh produkts
-		return "insert-page"; //paradis insert-page.html lapu
+
+	@GetMapping("/insert") // localhost:8080/insert
+	public String insertProductFunc(Product product) { // tiek padots tuksh produkts
+		return "insert-page"; // paradis insert-page.html lapu
 	}
-	
+
 	@PostMapping("/insert")
-	public String insertProductPostFunc(Product product) {  //tiek sanemts aizpildits produkts
-		Product prod = new Product(product.getTitle(), product.getPrice(), product.getDescription(), product.getQuantity());
+	public String insertProductPostFunc(Product product) { // tiek sanemts aizpildits produkts
+		Product prod = new Product(product.getTitle(), product.getPrice(), product.getDescription(),
+				product.getQuantity());
 		allProducts.add(prod);
 		return "redirect:/allProducts"; // izsaucam get kontrolieri localhost:8080/allProducts
 	}
-	//TODO update
-	//TODO izveidot get kontrolieri, kas nolasiis produkta id un pec ta atradis produktu un nosutiis caur model objektu uz frontent
+
+	// TODO update
+	// TODO izveidot get kontrolieri, kas nolasiis produkta id un pec ta atradis
+	// produktu un nosutiis caur model objektu uz frontent
 	// + paradit update page
-	@GetMapping("/update/{id}") //localhost8080:/update/1
+	@GetMapping("/update/{id}") // localhost8080:/update/1
 	public String updateProductByIdGetFunc(@PathVariable("id") int id, Model model) {
-		for(Product temp: allProducts) {
-			if(temp.getId() == id) {
-				model.addAttribute("product",temp);
+		for (Product temp : allProducts) {
+			if (temp.getId() == id) {
+				model.addAttribute("product", temp);
 				return "update-page";
 			}
 		}
 		return "error-page";
 	}
-	
-	//TODO izveidot update-page.html, kas stradas uz cita endpointa
-	//TODO izveidot post kontrolieri, kas sanemto objektu redige ari allProducts sarakstaa
+
+	// TODO izveidot update-page.html, kas stradas uz cita endpointa
+	// TODO izveidot post kontrolieri, kas sanemto objektu redige ari allProducts
+	// sarakstaa
 	@PostMapping("/update/{id}")
-	public String updateProductByIdPostFunc(@PathVariable("id")int id, Product product) { //ienak redigetais
-		for(Product temp: allProducts) {
-			if(temp.getId() == id) {
+	public String updateProductByIdPostFunc(@PathVariable("id") int id, Product product) { // ienak redigetais
+		for (Product temp : allProducts) {
+			if (temp.getId() == id) {
 				temp.setTitle(product.getTitle());
 				temp.setPrice(product.getPrice());
 				temp.setDescription(product.getDescription());
 				temp.setQuantity(product.getQuantity());
-				return "redirect:/product/"+temp.getTitle(); //tiks izsaukts localhost:8080/product/Abols
+				return "redirect:/product/" + temp.getTitle(); // tiks izsaukts localhost:8080/product/Abols
 			}
 		}
-		return "redirect:/error"; //tiks izsaukts localhost:8080/error
+		return "redirect:/error"; // tiks izsaukts localhost:8080/error
 	}
+
 	@GetMapping("/error")
 	public String errorFunc() {
 		return "error-page";
 	}
-	//TODO izveidot delete funkcionalitati
+
+	// TODO izveidot delete funkcionalitati
 	@GetMapping("/delete/{id}")
-	public String deleteProductById(@PathVariable("id")int id, Model model) {
-		for(Product temp : allProducts) {
-			if(temp.getId() == id) {
+	public String deleteProductById(@PathVariable("id") int id, Model model) {
+		for (Product temp : allProducts) {
+			if (temp.getId() == id) {
 				allProducts.remove(temp);
 				model.addAttribute("myAllProducts", allProducts);
 				return "all-products-page"; // parada all-products-page.html lapu
@@ -147,5 +153,5 @@ public class FirstController {
 		}
 		return "error-page";
 	}
-	
+
 }
---
+++ src/main/java/lv/venta/services/impl/ProductServiceImpl.java
@@ -0,0 +1,97 @@
+package lv.venta.services.impl;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import org.springframework.stereotype.Service;
+
+import lv.venta.models.Product;
+import lv.venta.services.iCRUDProductService;
+import lv.venta.services.iFilteringProductService;
+
+@Service
+public class ProductServiceImpl implements iCRUDProductService, iFilteringProductService {
+
+	private ArrayList<Product> allProducts = new ArrayList<>(Arrays.asList(new Product("Abols", 3.99f, "Sarkans", 3),
+			new Product("Burkans", 0.33f, "Oranzs", 2), new Product("Gurkis", 1.22f, "Zals", 6)));
+
+	@Override
+	public ArrayList<Product> retrieveAllProducts() {
+		return allProducts;
+	}
+
+	@Override
+	public Product retrieveOneProductById(int id) throws Exception {
+		for (Product temp : allProducts) {
+			if (temp.getId() == id) {
+				return temp;
+			}
+		}
+		throw new Exception("Wrong id");
+	}
+
+	// TODO
+	@Override
+	public Product retrieveOneProductByTitle(String title) throws Exception {
+		for (Product temp : allProducts) {
+			if (temp.getTitle().equals(title)) {
+				return temp;
+			}
+		}
+		throw new Exception("Wrong Title");
+	}
+
+	@Override
+	public Product insertProductByParams(String title, float price, String description, int quantity) {
+		for (Product temp : allProducts) {
+			if (temp.getTitle().equals(title) && temp.getPrice() == price
+					&& temp.getDescription().equals(description)) {
+				temp.setQuantity(temp.getQuantity() + quantity);
+				return temp;
+			}
+		}
+		Product newProduct = new Product(title, price, description, quantity);
+		allProducts.add(newProduct);
+		return newProduct;
+	}
+
+	@Override
+	public Product updateProductByParams(int id, String title, float price, String description, int quantity)
+			throws Exception {
+		for (Product temp : allProducts) {
+			if (temp.getId() == id) {
+				temp.setTitle(title);
+				temp.setPrice(price);
+				temp.setDescription(description);
+				temp.setQuantity(quantity);
+				return temp;
+			}
+		}
+		throw new Exception("Wrong id");
+	}
+
+	@Override
+	public void deleteProductById(int id) throws Exception {
+		for (Product temp : allProducts) {
+			if (temp.getId() == id) {
+				allProducts.remove(temp);
+			}
+		}
+		throw new Exception("Wrong id");
+	}
+
+	@Override
+	public ArrayList<Product> filterByPriceLess(float price) {
+		if (price > 0) {
+			ArrayList<Product> allProductsWithPriceLess = new ArrayList<>();
+			for (Product temp : allProducts) {
+				if (temp.getPrice() < price) {
+					allProductsWithPriceLess.add(temp);
+
+				}
+			}
+			return allProductsWithPriceLess;
+		}
+		return new ArrayList();
+	}
+}
