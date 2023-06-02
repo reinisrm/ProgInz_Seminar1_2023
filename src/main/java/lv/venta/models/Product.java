786992edeeae862fa1704ff68bc9990a1896cf90
reinisrm <rreeiinniiss@gmail.com>
2023-06-02 23:47:57 +0300

    New: Added validation in model class

--- src/main/java/lv/venta/models/Product.java
+++ src/main/java/lv/venta/models/Product.java
@@ -1,11 +1,34 @@
 package lv.venta.models;
 
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+
 public class Product {
+	
         private int id;
+        
+        @NotNull
+        @Pattern(regexp = "[A-Z] {1} [a-z\\ ]+")
+        @Size(min = 3, max = 30)
         private String title;
+        
+        @Min(value = 0)
+        @Max(value = 10000)
         private float price;
+        
+        @NotNull
+        @Pattern(regexp = "[A-Z] {1} [a-z\\ ]+")
+        @Size(min = 3, max = 30)   
         private String description;
+        
+        @Min(value = 0)
+        @Max(value = 1000)
         private int quantity;
+        
+        
         private static int idCounter = 0;
         public int getId() {
                 return id;
