227d0c0e24bd1e64edbd099f5613e97a22937b4e
reinisrm <rreeiinniiss@gmail.com>
2023-06-03 23:36:26 +0300

    Edit: change service related to DB

---
+++ src/main/java/lv/venta/services/impl/ProductServiceImplWithDB.java
@@ -0,0 +1,71 @@
+package lv.venta.services.impl;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import lv.venta.models.Product;
+import lv.venta.repo.iProductRepo;
+import lv.venta.services.iCRUDProductService;
+import lv.venta.services.iFilteringProductService;
+
+@Service
+public class ProductServiceImplWithDB implements iCRUDProductService, iFilteringProductService {
+	
+	@Autowired
+	private iProductRepo productRepo;
+	
+	@Override
+	public ArrayList<Product> filterByPriceLess(float price) {
+		// TODO Auto-generated method stub
+		return productRepo.findByPriceLessThan(price);
+	}
+
+	@Override
+	public ArrayList<Product> retrieveAllProducts() {
+		// TODO Auto-generated method stub
+		return (ArrayList<Product>) productRepo.findAll();
+	}
+
+	@Override
+	public Product retrieveOneProductById(int id) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Product retrieveOneProductByTitle(String title) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Product insertProductByParams(String title, float price, String description, int quantity) {
+		Product temp = productRepo.save(new Product(title, price, description, quantity));
+		return temp;
+	}
+
+	@Override
+	public Product updateProductByParams(int id, String title, float price, String description, int quantity) 
+			throws Exception {
+		
+		if(productRepo.existsById(id)) { //Parbaudam vai eksiste tads produkts ar id db
+			Product updatedPr = productRepo.findById(id).get(); // pie find by id vienmer vajag vel get()
+			updatedPr.setTitle(title);
+			updatedPr.setDescription(description);
+			updatedPr.setPrice(price);
+			updatedPr.setQuantity(quantity);
+			return productRepo.save(updatedPr);
+		} else {
+			throw new Exception("Wrong id");
+		}		
+	}
+
+	@Override
+	public void deleteProductById(int id) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
